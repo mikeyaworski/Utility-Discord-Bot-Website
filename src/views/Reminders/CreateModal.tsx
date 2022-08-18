1adab3275f680065e891a05658794bd0dec2864a
Michael Yaworski <mikeyaworski@hotmail.com>
2022-08-18 03:57:02 -0400

    Uses bot DM channel for reminders if no guild is selected

--- src/views/Reminders/CreateModal.tsx
+++ src/views/Reminders/CreateModal.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import BaseModal, { BaseModalProps } from 'modals/Base';
 import { ReminderModel } from 'types';
 import { Typography } from '@mui/material';
+import { GuildContext } from 'contexts/guild';
+import { AuthContext } from 'contexts/auth';
 import { convertReactMentionsToDiscordMentions } from 'utils';
 import ModalInputs from './ModalInputs';
 
@@ -17,12 +19,18 @@ const CreateReminderModal: React.FC<Props> = ({
   onConfirm,
   ...baseModalProps
 }) => {
+  const { selectedGuildId } = useContext(GuildContext);
+  const { botDmChannelId } = useContext(AuthContext);
+
   const [message, setMessage] = useState<string>('');
   const [time, setTime] = useState<number | null>(null);
   const [interval, setInterval] = useState<number | null>(null);
   const [endTime, setEndTime] = useState<number | null>(null);
   const [maxOccurrences, setMaxOccurrences] = useState<number | null>(null);
-  const [channelId, setChannelId] = useState<string | null>(null);
+  const [channelId, setChannelId] = useState<string | null>(() => {
+    if (!selectedGuildId && botDmChannelId) return botDmChannelId;
+    return null;
+  });
 
   const canConfirm = Boolean(time) && Boolean(channelId);
 
