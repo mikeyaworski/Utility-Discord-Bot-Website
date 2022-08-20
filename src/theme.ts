be9e38f20675500fa0a81e1d315ab694430cad2e
Michael Yaworski <mikeyaworski@hotmail.com>
2022-08-19 22:41:20 -0400

    Changes theme color to green

--- src/theme.ts
+++ src/theme.ts
@@ -3,5 +3,9 @@ import { createTheme } from '@mui/material';
 export default createTheme({
   palette: {
     mode: 'dark',
+    primary: {
+      main: '#00A651',
+      contrastText: '#FFFFFF',
+    },
   },
 });
