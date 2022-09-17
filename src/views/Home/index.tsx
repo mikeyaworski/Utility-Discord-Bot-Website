04fd9995b91698588667299f764211aa31a591b6
DESKTOP-V3FPRIF\mikeyaworski <mikeyaworski@hotmail.com>
2022-09-17 15:36:06 -0400

    Prevents home screen from rendering when still fetching user

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Paper, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import { AuthContext } from 'contexts/auth';
 import { routes } from 'components/Navigation/Sidebar';
 
 const Home: React.FC = () => {
+  const { hasFetched } = useContext(AuthContext);
   const navigate = useNavigate();
+  if (!hasFetched) return null;
   return (
     <Paper sx={{ width: '100%', maxWidth: 800, mx: 'auto' }}>
       <List sx={{ width: '100%' }}>
