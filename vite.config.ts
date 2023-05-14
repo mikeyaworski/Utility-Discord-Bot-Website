68f87e639473e7158c270361fd3480ef172a7cad
Michael Yaworski <mikeyaworski@hotmail.com>
2023-05-14 18:21:32 -0400

    Updates Vite config to use env variable port

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react(), tsconfigPaths()],
+    server: {
+      port: Number(env.PORT) || 8080,
+    },
+  };
 });
