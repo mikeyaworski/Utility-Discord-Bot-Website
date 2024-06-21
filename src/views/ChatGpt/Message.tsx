fe42000986a07f457f54b3ccf7056792108f26f9
IzqCode <84123307+IzqCode@users.noreply.github.com>
2024-06-21 15:18:18 -0600

    [ChatGPT] Adds markdown rendering (#3)
    Renders ChatGPT messages with react-markdown
    Supports LaTeX rendering as well

--- src/views/ChatGpt/Message.tsx
+++ src/views/ChatGpt/Message.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
-import { Chip, Typography } from '@mui/material';
+import Markdown from 'react-markdown';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
+import remarkGfm from 'remark-gfm';
+import { Chip, Box } from '@mui/material';
 import { Delete as TrashIcon } from '@mui/icons-material';
 import type { ChatGptConversationMessage } from 'types';
 import { useContextMenu } from 'hooks';
+
+import 'katex/dist/katex.min.css';
+
+// https://github.com/remarkjs/react-markdown/issues/785#issuecomment-1966495891
+const preprocessLaTeX = (content: string) => {
+  // Replace block-level LaTeX delimiters \[ \] with $$ $$
+  const blockProcessedContent = content.replace(
+    /\\\[(.*?)\\\]/gs,
+    (_, equation) => `$$${equation}$$`,
+  );
+  // Replace inline LaTeX delimiters \( \) with $ $
+  const inlineProcessedContent = blockProcessedContent.replace(
+    /\\\((.*?)\\\)/gs,
+    (_, equation) => `$${equation}$`,
+  );
+  return inlineProcessedContent;
+};
 
 interface Props {
   message: ChatGptConversationMessage,
@@ -25,17 +46,42 @@ const Message: React.FC<Props> = ({ message, onDelete }) => {
     <>
       <Chip
         onContextMenu={handleContextMenu}
-        label={<Typography variant="body1" sx={{ cursor: 'context-menu' }}>{message.content}</Typography>}
+        label={(
+          <Box
+            sx={{
+              cursor: 'context-menu',
+              '& p': {
+                whiteSpace: 'pre-wrap',
+              },
+              '& p, ol, ul': {
+                my: 0.5,
+              },
+              '& h1, h2, h3': {
+                my: 1.5,
+              },
+              '& h4, h5, h6': {
+                my: 1,
+              },
+              '& pre': {
+                my: 1.5,
+              },
+            }}
+          >
+            <Markdown remarkPlugins={[remarkMath, remarkGfm]} rehypePlugins={[rehypeKatex]}>{preprocessLaTeX(message.content)}
+            </Markdown>
+          </Box>
+        )}
         color={message.role === 'assistant' ? 'default' : 'primary'}
         sx={{
+          fontSize: '1rem', // Prevent the Chip styling from shrinking text since this is not a normal Chip
           alignSelf: message.role === 'assistant' ? 'flex-start' : 'flex-end',
           maxWidth: '95%',
           // multiline
           height: 'auto',
           '& .MuiChip-label': {
             display: 'block',
-            whiteSpace: 'pre-wrap',
-            py: '8px',
+            whiteSpace: 'normal',
+            py: 0.5,
           },
         }}
       />
