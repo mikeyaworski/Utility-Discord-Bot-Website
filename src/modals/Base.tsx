3048dd63a2600d4a8f77a6ee3b0d463a4d982186
Michael Yaworski <mikeyaworski@hotmail.com>
2024-07-23 01:55:13 -0400

    Feat: Add form submission support in modals

--- src/modals/Base.tsx
+++ src/modals/Base.tsx
@@ -12,6 +12,7 @@ interface Props {
   canConfirm?: boolean,
   children: React.ReactNode,
   disableBackdropDismissal?: boolean,
+  allowFormSubmission?: boolean,
 }
 
 export type BaseModalProps = Omit<Props, 'children'>;
@@ -26,8 +27,45 @@ const BaseModal: React.FC<Props> = ({
   busy = false,
   canConfirm = true,
   disableBackdropDismissal = false,
+  allowFormSubmission = false,
   children,
 }) => {
+  const body = (
+    <Box sx={{
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      width: 400,
+      bgcolor: 'background.paper',
+      border: '2px solid #000',
+      boxShadow: 24,
+      p: 4,
+      maxHeight: '95vh',
+      overflow: 'auto',
+    }}
+    >
+      {children}
+      <Divider sx={{ my: 2 }} />
+      <Box sx={{ display: 'flex', justifyContent: 'flex-end', gap: 2 }}>
+        <Button color="inherit" onClick={onClose} disabled={busy} variant="outlined">
+          Cancel
+        </Button>
+        <Button
+          color={confirmColor}
+          startIcon={confirmIcon || (busy && (
+            <CircularProgress size={14} color="inherit" />
+          ))}
+          type={allowFormSubmission ? 'submit' : undefined}
+          onClick={allowFormSubmission ? undefined : onConfirm}
+          disabled={busy || !canConfirm}
+          variant="contained"
+        >
+          {confirmText}
+        </Button>
+      </Box>
+    </Box>
+  );
   return (
     <Modal
       open={open}
@@ -35,39 +73,15 @@ const BaseModal: React.FC<Props> = ({
         if (reason !== 'backdropClick' || !disableBackdropDismissal) onClose();
       }}
     >
-      <Box sx={{
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 400,
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        p: 4,
-        maxHeight: '95vh',
-        overflow: 'auto',
-      }}
-      >
-        {children}
-        <Divider sx={{ my: 2 }} />
-        <Box sx={{ display: 'flex', justifyContent: 'flex-end', gap: 2 }}>
-          <Button color="inherit" onClick={onClose} disabled={busy} variant="outlined">
-            Cancel
-          </Button>
-          <Button
-            color={confirmColor}
-            startIcon={confirmIcon || (busy && (
-              <CircularProgress size={14} color="inherit" />
-            ))}
-            onClick={onConfirm}
-            disabled={busy || !canConfirm}
-            variant="contained"
-          >
-            {confirmText}
-          </Button>
-        </Box>
-      </Box>
+      {allowFormSubmission ? (
+        <form onSubmit={e => {
+          e.preventDefault();
+          onConfirm();
+        }}
+        >
+          {body}
+        </form>
+      ) : body}
     </Modal>
   );
 };
