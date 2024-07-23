88875d277942538f8eb605a2dc7b1e7fa17681b5
Michael Yaworski <mikeyaworski@hotmail.com>
2024-07-23 02:03:22 -0400

    Feat: Add ButtonMenu component

---
+++ src/components/ButtonMenu.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Menu, MenuItem, IconButton } from '@mui/material';
+import { MoreHoriz as MoreHorizIcon } from '@mui/icons-material';
+
+interface MenuItem {
+  icon?: typeof MoreHorizIcon,
+  iconColor?: React.ComponentProps<typeof MoreHorizIcon>['color'],
+  label: string,
+  onClick: () => void,
+}
+
+interface Props {
+  icon?: typeof MoreHorizIcon,
+  iconSize?: number,
+  items: MenuItem[],
+  disabled?: boolean,
+}
+
+const ButtonMenu: React.FC<Props> = ({ icon: Icon = MoreHorizIcon, iconSize = 20, disabled, items }) => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <>
+      <IconButton
+        onClick={handleClick}
+        size="small"
+        disabled={disabled}
+      >
+        <Icon sx={{ width: iconSize, height: iconSize }} />
+      </IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        slotProps={{
+          paper: {
+            elevation: 0,
+            sx: {
+              overflow: 'visible',
+              filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+              mt: 1,
+              '&::before': {
+                content: '""',
+                display: 'block',
+                position: 'absolute',
+                top: 0,
+                right: 11,
+                width: 10,
+                height: 10,
+                bgcolor: 'background.paper',
+                transform: 'translateY(-50%) rotate(45deg)',
+                zIndex: 0,
+              },
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        {items.map(({ label, onClick, icon: Icon, iconColor }) => (
+          <MenuItem key={label} onClick={onClick}>
+            {Icon && <Icon fontSize="small" color={iconColor} sx={{ mr: 1.5 }} />} {label}
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
+  );
+};
+
+export default ButtonMenu;
